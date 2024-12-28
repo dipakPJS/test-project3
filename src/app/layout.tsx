05b09bd8b600dd0292aecffebeb856a2ca30443b
dipakPjs <deepakpokharel811@gmail.com>
2024-12-28 22:01:14 +0545

    some updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// lenis
+
+import { ReactLenis } from "@/utils/lenis";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,11 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <ReactLenis root>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
         {children}
       </body>
+        </ReactLenis>
     </html>
   );
 }
